e0ec64bc7947250a77912bc785543c6153b79324
iamaldren <aldrenbobiles@gmail.com>
2019-10-31 22:41:23 +0800

    Base for integration testing

--- src/test/java/com/aldren/messaging/AppTest.java
+++ src/test/java/com/aldren/messaging/AppTest.java
@@ -1,13 +1,30 @@
 package com.aldren.messaging;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.assertTrue;
 
 /**
- * Unit test for simple App.
+ * Integration test for the App
  */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class AppTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private MockMvc mockMvc;
+
     /**
      * Rigorous Test :-)
      */
