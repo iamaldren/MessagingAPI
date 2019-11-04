cd495f319b85168981a0bbceacd8831f22b9d56d
iamaldren <aldrenbobiles@gmail.com>
2019-11-04 21:38:52 +0800

    Added integration test for read and send endpoints

--- src/test/java/com/aldren/messaging/AppTest.java
+++ src/test/java/com/aldren/messaging/AppTest.java
@@ -2,7 +2,12 @@ package com.aldren.messaging;
 
 import com.aldren.messaging.document.Messages;
 import com.aldren.messaging.document.Users;
+import com.aldren.messaging.model.Message;
 import com.aldren.messaging.repository.UsersRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
@@ -18,7 +25,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Integration test for the App
@@ -26,6 +38,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Slf4j
 public class AppTest {
 
     @Autowired
@@ -49,12 +62,17 @@ public class AppTest {
     private static final String THOR_ODINSON = "thorodinson";
 
     private static final String ROLE_USER = "User";
+    private static final String X_USER_HEADER = "X-User";
 
     private static final String ACTIVE_STATUS = "ACTIVE";
 
     @Before
     public void setup() throws Exception {
         if(!IS_INITIALIZED) {
+            log.info(">>>>>>>>>>>>>>>>>>>>> SETUP <<<<<<<<<<<<<<<<<<<<<<<<<");
+            mongoTemplate.dropCollection(Users.class);
+            mongoTemplate.dropCollection(Messages.class);
+
             Users tonystark = new Users();
             tonystark.setUserId(TONY_STARK);
             tonystark.setFirstName("Tony");
@@ -108,8 +126,60 @@ public class AppTest {
     }
 
     @Test
-    public void shouldAnswerWithTrue() {
-        Users test = usersRepository.findByUserId(TONY_STARK);
-        assertThat(test.getUserId()).isEqualTo(TONY_STARK);
+    public void testReadAndSendEndpoint() throws Exception {
+        String subject1 = "Avengers Initiative";
+        String content1 = "Let's start the initiative, and start gathering members.";
+
+        Message message1 = new Message();
+        message1.setReceiver(STEVE_ROGERS);
+        message1.setSubject(subject1);
+        message1.setContent(content1);
+
+        mockMvc.perform(post("/api/v1/message/send")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(convertObject(message1))
+                .header(X_USER_HEADER, TONY_STARK))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is(HttpStatus.OK.value())))
+                .andExpect(jsonPath("$.description", is(HttpStatus.OK.name())))
+                .andExpect(jsonPath("$.message.receiver", is(STEVE_ROGERS)))
+                .andExpect(jsonPath("$.message.sender", is(TONY_STARK)));
+
+        String subject2 = "Avengers";
+        String content2 = "I heard there's an initiative to gather superpowered beings as Earth's protectors. I wanna join!";
+
+        Message message2 = new Message();
+        message2.setReceiver(STEVE_ROGERS);
+        message2.setSubject(subject2);
+        message2.setContent(content2);
+
+        mockMvc.perform(post("/api/v1/message/send")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(convertObject(message2))
+                .header(X_USER_HEADER, THOR_ODINSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status", is(HttpStatus.OK.value())))
+                .andExpect(jsonPath("$.description", is(HttpStatus.OK.name())))
+                .andExpect(jsonPath("$.message.receiver", is(STEVE_ROGERS)))
+                .andExpect(jsonPath("$.message.sender", is(THOR_ODINSON)));
+
+        mockMvc.perform(get("/api/v1/message/read")
+                .header(X_USER_HEADER, STEVE_ROGERS))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].subject", is(subject2)))
+                .andExpect(jsonPath("$[0].content", is(content2)))
+                .andExpect(jsonPath("$[0].sender", is(THOR_ODINSON)))
+                .andExpect(jsonPath("$[0].receiver", is(STEVE_ROGERS)))
+                .andExpect(jsonPath("$[1].subject", is(subject1)))
+                .andExpect(jsonPath("$[1].content", is(content1)))
+                .andExpect(jsonPath("$[1].sender", is(TONY_STARK)))
+                .andExpect(jsonPath("$[1].receiver", is(STEVE_ROGERS)));
     }
+
+    private String convertObject(Message message) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writer().withDefaultPrettyPrinter().writeValueAsString(message);
+    }
+
 }
